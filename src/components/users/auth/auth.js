0ab613812c968903fd733d8dc9f0613ef7047e6c
Hakan Yilmaz <hakanbien@gmail.com>
2022-05-20 12:55:52 +0200

    4.day finished

--- src/components/users/auth/auth.js
+++ src/components/users/auth/auth.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { Col, Container, Row, Tab, Tabs } from "react-bootstrap";
-import "./auth.css"
+import "./auth.css";
+import logo from "../../../assets/img/logo/logo.png";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+import LoginForm from "./login-form";
+import RegisterForm from "./register-form";
 
 const Auth = () => {
+  const navigate = useNavigate();
+
   return (
     <Container fluid className="auth">
       <Row>
-        <Col md={7} ></Col>
+        <Col md={7}>
+          <img src={logo} />
+          <RiCloseCircleLine
+            className="close-btn"
+            onClick={() => navigate(-1)}
+          />
+        </Col>
         <Col md={5}>
-          <Tabs
-            defaultActiveKey="login"
-            className="mb-3"
-          >
+          <Tabs defaultActiveKey="login" className="mb-3">
             <Tab eventKey="login" title="Login">
-            Login Form
+              <LoginForm/>
             </Tab>
-            <Tab eventKey="register" title="Register" disabled>
-            Register Key
+            <Tab eventKey="register" title="Register">
+              <RegisterForm/>
             </Tab>
           </Tabs>
         </Col>
