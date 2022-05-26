2e89c4e97dee73476bbe2ed1373de4f318e33bf4
Hakan Yilmaz <hakanbien@gmail.com>
2022-05-26 19:33:27 +0200

    Day 10

--- src/pages/users/UserReservationDetailsPage.js
+++ src/pages/users/UserReservationDetailsPage.js
@@ -1,17 +1,14 @@
 import React from "react";
 import PageHeader from "../../components/users/common/page-header/page-header";
 import Spacer from "../../components/users/common/spacer/spacer";
-import Vehicles from "../../components/users/vehicles/vehicles";
 
-const VehiclesPage = () => {
+const UserReservationDetailsPage = () => {
   return (
     <>
-      <PageHeader title="Vehicles" />
-      <Spacer />
-      <Vehicles />
+      <PageHeader title="Reservation Details" />
       <Spacer />
     </>
   );
 };
 
-export default VehiclesPage;
+export default UserReservationDetailsPage;
--- src/router/custom-routes.js
+++ src/router/custom-routes.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFoundPage from "../pages/common/NotFoundPage";
 import AboutPage from "../pages/users/AboutPage";
 import AuthPage from "../pages/users/AuthPage";
 import ContactPage from "../pages/users/ContactPage";
 import HomePage from "../pages/users/HomePage";
 import ProfilePage from "../pages/users/ProfilePage";
+import UserReservationDetailsPage from "../pages/users/UserReservationDetailsPage";
+import UserReservationsPage from "../pages/users/UserReservationsPage";
 import VehicleDetailsPage from "../pages/users/VehicleDetailsPage";
 import VehiclesPage from "../pages/users/VehiclesPage";
 import UserTemplate from "../templates/user-template";
+import ProtectedRoute from "./protected-route";
 
 const CustomRoutes = () => {
   return (
@@ -29,10 +33,14 @@ const CustomRoutes = () => {
           </Route>
 
           <Route path="user">
-            <Route index element={<UserTemplate><ProfilePage /></UserTemplate>} />
+            <Route index element={<ProtectedRoute><UserTemplate><ProfilePage /></UserTemplate></ProtectedRoute>} />
+            <Route path="reservations">
+              <Route index element={<ProtectedRoute><UserTemplate><UserReservationsPage/></UserTemplate></ProtectedRoute>} />
+              <Route path=":reservationId" element={<ProtectedRoute><UserTemplate><UserReservationDetailsPage/></UserTemplate></ProtectedRoute>} />
+            </Route>
           </Route>
 
-
+          <Route path='*' element={<UserTemplate><NotFoundPage /></UserTemplate>} />
         </Route>
       </Routes>
     </BrowserRouter>
